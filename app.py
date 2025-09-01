ed8118c41777d75271d2879c2527924164bbc038
David Owers <taff.vale.scc@gmail.com>
2025-09-01 19:31:13 +0100

    Fix port for Render deployment

--- app.py
+++ app.py
@@ -120,8 +120,8 @@ def logout():
     session.pop('username', None)
     return redirect('/')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
